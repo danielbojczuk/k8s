088333c6bf73a63bfee6fc96ceb1c77d6f9e6bac
Daniel Bojczuk <daniel@cebolinha.home>
2023-01-11 18:12:36 +0100

    adding script

---
+++ initNode.sh
@@ -0,0 +1,50 @@
+!$/bin/bash
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+modprobe overlay
+modprobe br_netfilter
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+sysctl --system
+
+swapoff -a
+sed -ri '/\sswap\s/s/^#?/#/' /etc/fstab
+
+mkdir -p /etc/apt/keyringsls
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+apt-get update
+
+apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
+
+apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+apt-get install -y kubelet kubeadm kubectl
+
+apt-mark hold kubelet kubeadm kubectl
+
+systemctl enable docker.service
+
+systemctl enable containerd.service
+
+rm /etc/containerd/config.toml
+containerd config default | tee /etc/containerd/config.toml
+sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml  
+
+sudo service containerd restart
+sudo service kubelet restart  
